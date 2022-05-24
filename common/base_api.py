8eb26bc005e9bc511fbee2b65f3ca65e35f9c2bc
汪杰 <wangjie@wangjiedeMacBook-Pro.local>
2022-05-24 20:37:15 +0800

    重构登录测试用例

--- common/base_api.py
+++ common/base_api.py
@@ -1,20 +1,22 @@
-#!/usr/bin/python
+# !/usr/bin/python
 # -*- coding:utf-8 -*-
+import json
+
 import requests
 
 
-class BaseApi():
+class BaseApi:
     """基础类，提供公共方法"""
 
-    def send_(self, data):
+    def send_(self, data) -> json:
         """发送请求，返回json格式数据"""
         r = requests.request(**data)
         return r.json()
 
-    def delete_(self):
+    def delete_(self) -> json:
         """删除请求，返回json格式数据"""
         pass
 
-    def put_(self):
+    def put_(self) -> json:
         """修改请求，返回json格式数据"""
         pass
---
+++ conftest.py
@@ -0,0 +1,6 @@
+# !/usr/bin/python
+# -*- coding:utf-8 -*-
+# @Time : 2022/5/24 18:11
+# @Author : wangjie
+# @File : conftest.py
+# @project : SensoroApi
