d6a50429f4f3c1ca8c39e8f7054f4a3ce4d0f5d0
汪杰 <1641540482@qq.com>
2023-06-01 17:47:47 +0800

    优化生成测试结果统计文件的方法

--- conftest.py
+++ conftest.py
@@ -132,7 +132,10 @@ def pytest_runtest_makereport(item, call):  # description取值为用例说明__
 
 def pytest_terminal_summary(terminalreporter, exitstatus, config):
     """收集测试结果展示在控制台，并发送到企业微信"""
-    with open(BASE_DIR + '/outFiles/pytest_result/pytest_result.json', 'r', encoding='utf-8') as f:
+    result_dir = BASE_DIR + '/outFiles/pytest_result'
+    if not os.path.exists(result_dir):
+        os.makedirs(result_dir, exist_ok=True)
+    with open(result_dir + '/pytest_result.json', 'r', encoding='utf-8') as f:
         pytest_result = json.loads(f.read())
         total_case = pytest_result['case_count']
         pass_case = pytest_result["case_pass"]
